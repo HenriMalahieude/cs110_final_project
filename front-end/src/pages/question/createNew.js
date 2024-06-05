814ad4e60a274e7ec00cf43729ed9cd2de284727
HenriMalahieude <henrimalahieude@gmail.com>
2024-06-05 04:05:39 -0700

    Created new. just need to test backend

--- front-end/src/pages/question/createNew.js
+++ front-end/src/pages/question/createNew.js
@@ -1,9 +1,41 @@
-import React from "react";
+import './new.css'
+import './question.css'
 
-export function QuestionCreation() {
+import { useState } from "react";
+import axios from 'axios';
+
+export function QuestionCreation({token}) {
+	const [title, setTitle] = useState('');
+	const [content, setContent] = useState('');
+
+	function Post() {
+		if (title == undefined || title.length <= 0) return;
+		if (content == undefined || content.length <= 0) return;
+
+		let question = {
+			title: title,
+			content: content,
+		}
+
+		axios.post(`http://localhost:8080/post/new`, question, {header: {Authorization: `Bearer ${token}`}})
+			 .then((data) => {
+				window.location.href = `http://localhost:3000/question/${data.questionId}`;
+			 })
+			 .catch((err) => {
+				console.log(err);
+			 })
+	}
+
 	return (<>
-		<div>
-			TODO
+		<div className="post-container">
+			<div className="post-title">
+				<div>Title: </div>
+				<input type="text" placeholder="required" className="pt-txtbox" onChange={(e) => {setTitle(e.target.value)}} value={title} />
+			</div>
+			<textarea className="post-content-txtbox" onChange={(e) => setContent(e.target.value)} value={content} />
+			<div className="post-confirm">
+				<button onClick={(e) => {Post()}}>Post</button>
+			</div>
 		</div>
 	</>);
 }
